7d436e2c8b1c68d8a42ba5ad02cb2bd07f785428
DESKTOP-AVTI26A\burto <matthewburton@webster.edu>
2020-07-07 16:51:20 -0500

    Just goofin'

---
+++ Superstore-Survival-Unity/Assets/RotorSpinner.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotorSpinner : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(new Vector3 (0,0,1) * (500 * Time.deltaTime));
+    }
+}
---
+++ Superstore-Survival-Unity/Assets/gun.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gun : MonoBehaviour
+{
+    private RaycastHit hit;
+
+    void Update()
+    {
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 100, Color.magenta);
+
+        Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward) * 100, out hit, Mathf.Infinity);
+
+        print(hit.transform);
+    }
+}
