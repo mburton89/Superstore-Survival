70007b39dc4ce6a07815d5b8143158cdd6b2d515
jaruble16 <55810537+jaruble16@users.noreply.github.com>
2020-10-15 14:44:51 -0500

    Finished Adding In The Main Menu And The Customization Scenes

---
+++ Superstore-Survival-Unity/Assets/MyAssets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private Scene scene;
+
+    private void Start()
+    {
+        scene = SceneManager.GetActiveScene();
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Additive);
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(scene.name);
+    }
+}
