8462ee193f0275df97ff7ff5cb98eb0867908e0d
doriangreen62 <71105367+doriangreen62@users.noreply.github.com>
2020-10-29 15:30:12 -0500

    Keys and Doors

---
+++ Superstore-Survival-Unity/Assets/DoorKey.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class DoorKey : MonoBehaviour
+{
+
+    public bool inTrigger = false;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.GetComponent<FirstPersonController>())
+        {
+            inTrigger = true;
+        }
+        
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        print("exit");
+        if (other.gameObject.GetComponent<FirstPersonController>())
+        {
+           
+            inTrigger = false;
+        }
+    }
+
+    void Update()
+    {
+        print(inTrigger);
+        if (inTrigger = true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                DoorScript.doorKey = true;
+                Destroy(this.gameObject);
+            }
+        }
+    }
+    private bool _canCollectKey = true;
+
+    private IEnumerator InTriggerBuffer()
+    {
+        _canCollectKey = false;
+        yield return new WaitForSeconds(1);
+        _canCollectKey = true;
+    }
+}
+
+
+    /*void OnGUI()
+    {
+        if (inTrigger)
+        {
+            GUI.Box(new Rect(0, 60, 200, 25), "Press E to take key");
+        }
+    }
+}*/
---
+++ Superstore-Survival-Unity/Assets/DoorScript.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+
+    public static bool doorKey = false;
+    public bool open;
+    public bool close;
+    public bool inTrigger;
+    public Transform hinge;
+    void OnTriggerEnter(Collider other)
+    {
+        
+        inTrigger = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        inTrigger = false;
+    }
+
+    void Update()
+    {
+        if (inTrigger)
+        {
+            if (close)
+            {
+                if (doorKey)
+                {
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+                        open = true;
+                        close = false;
+                    }
+                }
+            }
+            else
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    close = true;
+                    open = false;
+                }
+            }
+        }
+
+        if (open)
+        {
+            print("open");
+            var newRot = Quaternion.RotateTowards(hinge.rotation, Quaternion.Euler(0.0f, -90.0f, 0.0f), Time.deltaTime * 200);
+            hinge.rotation = newRot;
+        }
+        else
+        {
+            var newRot = Quaternion.RotateTowards(hinge.rotation, Quaternion.Euler(0.0f, 0.0f, 0.0f), Time.deltaTime * 200);
+            hinge.rotation = newRot;
+        }
+    }
+}
+
+    /*void OnGUI()
+    {
+        if (inTrigger)
+        {
+            if (open)
+            {
+                GUI.Box(new Rect(0, 0, 200, 25), "Press E to close");
+            }
+            else
+            {
+                if (doorKey)
+                {
+                    GUI.Box(new Rect(0, 0, 200, 25), "Press E to open");
+                }
+                else
+                {
+                    GUI.Box(new Rect(0, 0, 200, 25), "Need a key!");
+                           
+            }
+        }
+    }
+}*/
