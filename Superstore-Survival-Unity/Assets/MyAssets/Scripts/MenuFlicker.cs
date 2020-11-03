7bfe75210d6e8e94fd5ec7d93ecd8db1dceb46c1
jaruble16 <55810537+jaruble16@users.noreply.github.com>
2020-11-03 17:30:19 -0600

    Created Flicker Effect for Main Menu

---
+++ Superstore-Survival-Unity/Assets/MyAssets/Scripts/MenuFlicker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuFlicker : MonoBehaviour
+{
+    public GameObject darkScreen;
+
+    int randomNumber;
+
+    private void Update()
+    {
+        randomNumber = Random.Range(1, 10);
+
+        if (randomNumber == 1)
+        {
+            darkScreen.SetActive(true);
+        }
+        else
+        {
+            darkScreen.SetActive(false);
+        }
+    }
+}
