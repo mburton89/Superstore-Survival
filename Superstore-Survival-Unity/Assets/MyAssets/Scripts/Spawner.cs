fa69a59a7a9850a33cdea4e314502ec13f8c2766
Zach Ridings <zridings@gmail.com>
2020-11-03 18:49:09 -0600

    Spawner Script

---
+++ Superstore-Survival-Unity/Assets/MyAssets/Scripts/Spawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] items;
+    public GameObject spawner;
+    private int rand;
+
+    private void Start()
+    {
+        rand = Random.Range(0, items.Length);
+        Instantiate(items[rand], gameObject.transform.position, Quaternion.identity);
+        Destroy(spawner);
+    }
+}
