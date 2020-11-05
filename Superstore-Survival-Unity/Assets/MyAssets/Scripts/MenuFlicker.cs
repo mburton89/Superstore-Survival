79e1d32b06f5c066740e5318adebf15fa7ba03b7
jaruble16 <55810537+jaruble16@users.noreply.github.com>
2020-11-05 12:46:32 -0600

    Added In All Enemies and Their Sounds

---
+++ Superstore-Survival-Unity/Assets/MyAssets/Scripts/MenuFlicker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuFlicker : MonoBehaviour
+{
+    public GameObject darkScreen;
+
+    int randomNumber;
+
+    private void Update()
+    {
+        randomNumber = Random.Range(1, 100);
+
+        if (randomNumber < 7)
+        {
+            darkScreen.SetActive(true);
+        }
+        else
+        {
+            darkScreen.SetActive(false);
+        }
+    }
+}
