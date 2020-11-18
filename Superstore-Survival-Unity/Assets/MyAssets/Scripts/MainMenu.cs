91a42cb59746956009e45f52ecb352c8804ab45c
Zach Ridings <zridings@gmail.com>
2020-11-18 14:41:44 -0600

    Fading Load Screen

--- Superstore-Survival-Unity/Assets/MyAssets/Scripts/MainMenu.cs
+++ Superstore-Survival-Unity/Assets/MyAssets/Scripts/MainMenu.cs
@@ -9,10 +9,14 @@ public class MainMenu : MonoBehaviour
     private Scene scene;
     AsyncOperation loadingOperation;
     public Slider progressBar;
+    public Text percentLoaded;
+    public CanvasGroup customizationPanel;
 
     private void Update()
     {
         progressBar.value = Mathf.Clamp01(loadingOperation.progress / 0.9f);
+        float progressValue = Mathf.Clamp01(loadingOperation.progress / 0.9f);
+        percentLoaded.text = Mathf.Round(progressValue * 100) + "%";
     }
     private void Start()
     {
@@ -22,6 +26,22 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         loadingOperation = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(FadeLoad(0.25f));
+    }
+
+    IEnumerator FadeLoad(float duration)
+    {
+        float startValue = customizationPanel.alpha;
+        float time = 0;
+
+        while (time < duration)
+        {
+            customizationPanel.alpha = Mathf.Lerp(startValue, 0, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        customizationPanel.alpha = 0;
     }
 
     public void QuitGame()
