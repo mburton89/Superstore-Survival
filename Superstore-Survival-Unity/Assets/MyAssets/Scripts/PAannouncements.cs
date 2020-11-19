08bb8829ec618f4cdc5330a4c912290129614fb9
jaruble16 <55810537+jaruble16@users.noreply.github.com>
2020-11-19 13:13:20 -0600

    Added Final Enemy Sounds And Adjusted Their Volume

---
+++ Superstore-Survival-Unity/Assets/MyAssets/Scripts/PAannouncements.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PAannouncements : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    [SerializeField] AudioClip[] announcement;
+ 
+    float delayNumber;
+
+    private void Start()
+    {
+
+    }
+
+    public void PlayAnnouncement()
+    {
+        Delay();
+
+        if (delayNumber <= 0)
+        {
+            audioSource.Play();
+        }
+    }
+
+    public void Delay()
+    {
+        delayNumber = Random.Range(20, 40);
+
+        delayNumber -= Time.deltaTime;
+
+        if (delayNumber <= 0)
+        {
+            delayNumber = 0;
+        }
+    }
+}
